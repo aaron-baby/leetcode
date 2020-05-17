b0643ad1636e1e35892c30e02e1d91961ab4ff76
Wei Wang <aaron.ido@gmail.com>
2020-05-17 12:48:33 +0800

    correct solution

--- valid_palindrome.go
+++ valid_palindrome.go
@@ -1,32 +1,29 @@
 package main
 
-import "fmt"
-
 func validPalindrome(s string) bool {
-	return recursive_check(s, 0)
+	var i, j =0, len(s)-1
+	for i<j{
+		if s[i] == s[j]{
+			i++
+			j--
+		} else {
+			// remove char at position i or j
+			return isPalindrome(s[i+1:j+1]) || isPalindrome(s[i:j])
+		}
+	}
+
+	return true
 }
 
-func recursive_check(s string, c int) bool {
-	l := len(s)
-	fmt.Println(s)
-	if l == 1 {
-		return true
-	} else if l == 2 {
-		return s[0] == s[1]
+func isPalindrome(s string) bool {
+	var i, j =0, len(s)-1
+	for i<j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
 	}
-	if c > 1 {
-		return false
-	}
-	// compare first and last
-	if s[0] == s[l-1] {
-		return recursive_check(s[1:l-1], c)
-	} else {
-		c += 1
-		return recursive_check(s[1:l], c) || recursive_check(s[0:l-1], c)
-	}
-}
-func main() {
-	//fmt.Println(validPalindrome("aba"))
-	//fmt.Println(validPalindrome("abca"))
-	fmt.Println(validPalindrome("abcad"))
+
+	return true
 }
---
+++ valid_palindrome_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestValidPalindrome(t *testing.T) {
+	cases := map[string]bool{
+		"abc":  false,
+		"abca": true,
+		"a": true,
+	}
+
+	for s, want := range cases {
+		got := validPalindrome(s)
+		if got != want {
+			t.Errorf("string %s: got %t want %t", s, got, want)
+		}
+	}
+}
