65b9cbeeec74609c653c98024f6a7db423a24793
Wei Wang <aaron.ido@gmail.com>
2020-05-19 09:19:11 +0800

    add graph

--- graph/graph.go
+++ graph/graph.go
@@ -7,7 +7,29 @@ type (
 		Name         string
 		dependencies int
 	}
+
+	Graph struct {
+		nodes     []ItemGraph
+		valueItem map[string]ItemGraph
+	}
 )
+
+func (g *Graph) getOrCreateNode(name string) ItemGraph {
+	if node, ok := g.valueItem[name]; ok {
+		return node
+	}
+	node := ItemGraph{Name: name}
+	g.nodes = append(g.nodes, node)
+	g.valueItem[name] = node
+
+	return node
+}
+
+func (g *Graph) addEdge(startName string, endName string) {
+	start := g.getOrCreateNode(startName)
+	end := g.getOrCreateNode(endName)
+	start.AddNeighbor(end)
+}
 
 func (i *ItemGraph) AddNeighbor(item ItemGraph) {
 	if _, ok := i.valueItem[item.Name]; !ok {
