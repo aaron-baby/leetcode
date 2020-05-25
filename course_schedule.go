e32d6f50b05bd1db503921ab3205b54d4ead04c6
Wei Wang <aaron.ido@gmail.com>
2020-05-25 16:19:54 +0800

    resolve topological sort

---
+++ course_schedule.go
@@ -0,0 +1,33 @@
+package main
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	indegrees := make(map[int]int)
+	adjacencies := map[int][]int{}
+	for _, edge := range prerequisites {
+		start, end := edge[1], edge[0]
+		indegrees[end]++
+		adjacencies[start] = append(adjacencies[start], end)
+	}
+	// Create a queue and enqueue all vertices with
+	// indegree 0
+	var q []int
+	for i := 0; i < numCourses; i++ {
+		if indegrees[i] == 0 {
+			q = append(q, i)
+		}
+	}
+
+	var course, visited int
+	for len(q) > 0 {
+		course, q = q[len(q)-1], q[:len(q)-1]
+		for _, neighbour := range adjacencies[course] {
+			indegrees[neighbour]--
+			if indegrees[neighbour] == 0 {
+				q = append(q, neighbour)
+			}
+		}
+		visited++
+	}
+
+	return visited == numCourses
+}
---
+++ course_schedule_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestCanFinish(t *testing.T) {
+	numCourses := 2
+	prerequisites := [][]int{
+		{1, 0},
+		{0, 1},
+	}
+	got := canFinish(numCourses, prerequisites)
+	want := false
+	if got != want {
+		t.Errorf("got %t want %t", got, want)
+	}
+}
