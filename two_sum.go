f44c6258b5f457429b0e3d2ccf12dc6c0d167d36
Wei Wang <aaron.ido@gmail.com>
2020-05-25 19:09:46 +0800

    two sum hash solution

---
+++ two_sum.go
@@ -0,0 +1,17 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	// hash that remember element index
+	indices := make(map[int]int)
+	for i, v := range nums {
+		difference := target - v
+		j, ok := indices[difference]
+		if ok {
+			return []int{j, i}
+		} else {
+			indices[v] = i
+		}
+	}
+
+	return []int{}
+}
---
+++ two_sum_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	got := twoSum(nums, target)
+	want := []int{0, 1}
+	if !cmp.Equal(got, want) {
+		t.Errorf("got %v want %v given", got, want)
+	}
+}
