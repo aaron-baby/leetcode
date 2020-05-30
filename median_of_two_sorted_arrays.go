156d200873b23c8a72c8206d57af28d0aa1666d8
Wei Wang <aaron.ido@gmail.com>
2020-05-30 19:51:13 +0800

    median of two sorted arrays

---
+++ median_of_two_sorted_arrays.go
@@ -0,0 +1,58 @@
+package main
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	// let nums1 be small slice
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	m, n := len(nums1), len(nums2)
+	iMin, iMax, halfLen := 0, m, (m+n+1)/2
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && nums2[j-1] > nums1[i] {
+			iMin = i + 1
+		} else if i > iMin && nums1[i-1] > nums2[j] {
+			iMax = i - 1
+		} else {
+			// perfect situation
+			var maxLeft int
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = max(nums1[i-1], nums2[j-1])
+			}
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			var minRight int
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = min(nums1[i], nums2[j])
+			}
+			return float64(maxLeft+minRight) / 2.0
+		}
+	}
+
+	return 0.0
+}
---
+++ median_of_two_sorted_arrays_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestFindMedianSortedArrays(t *testing.T) {
+	nums1 := []int{1, 2}
+	//nums2 := []int{2}
+	nums2 := []int{3, 4}
+	got := findMedianSortedArrays(nums1, nums2)
+	//want := 2.0
+	want := 2.5
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
