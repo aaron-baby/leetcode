b3c957f6af4475920237fac59f7a3893b8102be1
Wei Wang <aaron.ido@gmail.com>
2020-06-04 16:32:53 +0800

    n queens solution

---
+++ n_queens.go
@@ -0,0 +1,60 @@
+package main
+
+var leftDiagonal, rightDiagonal, cl []int
+
+func solveNQueens(n int) [][]string {
+	slt := [][]string{}
+	leftDiagonal, rightDiagonal, cl = make([]int, 2*n-1), make([]int, 2*n-1), make([]int, n)
+	board := make([][]int, n)
+	// column left to right
+	for j := 0; j < n; j++ {
+		board[j] = make([]int, n)
+	}
+	solveNQueenRecursive(board, 0, &slt)
+
+	return slt
+}
+
+func printBoard(board [][]int) []string {
+	var r []string
+	for i := 0; i < len(board); i++ {
+		t := ""
+		for j := 0; j < len(board[i]); j++ {
+			if board[i][j] == 1 {
+				t += "Q"
+			} else {
+				t += "."
+			}
+		}
+		r = append(r, t)
+	}
+
+	return r
+}
+
+func solveNQueenRecursive(board [][]int, col int, slt *[][]string) bool {
+	n := len(board)
+	if col >= n {
+		return true
+	}
+	var res bool
+	for i := 0; i < n; i++ {
+		if leftDiagonal[i-col+n-1] != 1 && rightDiagonal[i+col] != 1 && cl[i] != 1 {
+			board[i][col] = 1
+			leftDiagonal[i-col+n-1], rightDiagonal[i+col] = 1, 1
+			cl[i] = 1
+
+			res = solveNQueenRecursive(board, col+1, slt)
+			if res {
+				*slt = append(*slt, printBoard(board))
+			}
+
+			// back track
+			board[i][col] = 0
+			leftDiagonal[i-col+n-1], rightDiagonal[i+col] = 0, 0
+			cl[i] = 0
+		}
+	}
+
+	return false
+}
---
+++ n_queens_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestSolveNQueens(t *testing.T) {
+	got := solveNQueens(2)
+	want := [][]string{}
+	if !cmp.Equal(got, want) {
+		t.Errorf("got %v want %v given", got, want)
+	}
+
+	// 4 dimensions
+	got = solveNQueens(4)
+	want = [][]string{
+		{
+			"..Q.",
+			"Q...",
+			"...Q",
+			".Q..",
+		},
+		{
+			".Q..",
+			"...Q",
+			"Q...",
+			"..Q.",
+		},
+	}
+	if !cmp.Equal(got, want) {
+		t.Errorf("got %v want %v given", got, want)
+	}
+}
