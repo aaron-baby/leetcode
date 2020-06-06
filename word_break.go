400e2df2105f7f6d82c799a67153f18d755bd4bb
Wei Wang <aaron.ido@gmail.com>
2020-06-06 12:28:59 +0800

    word break

---
+++ word_break.go
@@ -0,0 +1,23 @@
+package main
+
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+
+	for i := range s {
+		// word break lenght "","l","le"
+		if !dp[i] {
+			continue
+		}
+		for _, word := range wordDict {
+			if i+len(word) > len(s) {
+				continue
+			}
+			if s[i:i+len(word)] == word {
+				dp[i+len(word)] = true
+			}
+		}
+	}
+
+	return dp[len(s)]
+}
---
+++ word_break_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestWordBreak(t *testing.T) {
+	testCases := map[string][]string{
+		"leetcode":      []string{"leet", "code"},
+		"applepenapple": []string{"apple", "pen"},
+	}
+	wants := []bool{true, true}
+	i := 0
+	for s, wordDict := range testCases {
+		got := wordBreak(s, wordDict)
+		if got != wants[i] {
+			t.Errorf("string %s: got %t want %t", s, got, wants[i])
+		}
+		i++
+	}
+}
