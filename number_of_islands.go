32945c48963528fb78d3ede517cc0f33ef087eee
Wei Wang <aaron.ido@gmail.com>
2020-07-15 12:36:18 +0800

    200. Number of Islands

---
+++ number_of_islands.go
@@ -0,0 +1,32 @@
+package main
+
+//200. Number of Islands
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	// m for row size, n for column size
+	var m, n = len(grid), len(grid[0])
+	var ans int
+	for x := 0; x < m; x++ {
+		for y := 0; y < n; y++ {
+			if grid[x][y] == '1' {
+				ans += 1
+				dfs(grid, x, y, m, n)
+			}
+		}
+	}
+
+	return ans
+}
+
+func dfs(grid [][]byte, x, y, m, n int) {
+	if x < 0 || y < 0 || x >= m || y >= n || grid[x][y] == '0' {
+		return
+	}
+	grid[x][y] = '0'
+	dfs(grid, x+1, y, m, n)
+	dfs(grid, x-1, y, m, n)
+	dfs(grid, x, y-1, m, n)
+	dfs(grid, x, y+1, m, n)
+}
---
+++ number_of_islands_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestNumIslands(t *testing.T) {
+	grid := [][]byte{
+		{'1', '1', '1', '1', '0'},
+		{'1', '1', '0', '1', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '0', '0', '0'},
+	}
+	cnt := numIslands(grid)
+	want := 1
+	if cnt != want {
+		t.Errorf("got %v want %v given", cnt, want)
+	}
+
+	grid = [][]byte{
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
+	}
+	cnt = numIslands(grid)
+	want = 3
+	if cnt != want {
+		t.Errorf("got %v want %v given", cnt, want)
+	}
+
+	grid = [][]byte{}
+	cnt = numIslands(grid)
+	want = 0
+	if cnt != want {
+		t.Errorf("got %v want %v given", cnt, want)
+	}
+}
